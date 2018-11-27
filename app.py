6a3a8ec96ea4465c578adfe53e85aa1f99b2e198
Ada Chen <44806107+wanjuchen@users.noreply.github.com>
2018-11-27 10:16:56 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -17,19 +17,23 @@ def verify():
 		if not request.args.get("hub.verify_token") == VERIFICATION_TOKEN:
 			return "Verification token mismatch", 403
 		return request.args["hub.challenge"], 200
-	return "Hello world", 200
+	return "Hello new world", 200
 
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     if data['object']=='page':
         for entry in data['entry']:
+            my_id = entry['id']
             for msging_event in entry['messaging']:
+                print(msging_event)
                 s_id = msging_event['sender']['id']
                 if msging_event.get('message'):
                     if 'text' in msging_event['message']:
                         msg_text = msging_event['message']['text']
-                        newRply = get_news_elements(wit_response(msg_text,s_id),s_id)
+                        print('111111111')
+                        print(msg_text)
+                        newRply = get_news_elements(wit_response(msg_text,s_id),s_id,msg_text)
                     else:
                         msg_text = 'no text'
                     reply(s_id,newRply)
@@ -48,7 +52,6 @@ def reply(user_id, msg):
         "message": {"text": msg}
     }
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + PAGE_ACCESS_TOKEN_1, json=data)
-    print(resp.content)
 
 if __name__ == "__main__":
 	app.run(debug =True,port=3000)
